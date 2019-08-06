40750b99eb8e6f9ab962aaace208196b8b4a4e06
luis acerv <l.arr.cerv@gmail.com>
2019-08-05 22:26:49 -0500

    add sharing buttons

---
+++ src/components/ArticleSocial.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Twitter from "react-sharingbuttons/dist/buttons/Twitter";
+import Facebook from "react-sharingbuttons/dist/buttons/Facebook";
+
+export default ({ meta, title }) => {
+  const url = window.location;
+  const shareText = `${title}... #${meta.tags[0]} #${meta.tags[1]}  - ${
+    window.location
+  }`;
+
+  return (
+    <div>
+      <Twitter link={url} shareText={shareText} />
+      <Facebook link={url} shareText={shareText} />
+    </div>
+  );
+};
