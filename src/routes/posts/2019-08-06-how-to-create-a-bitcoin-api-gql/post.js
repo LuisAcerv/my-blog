ad6a0597e790dea1bcb37c06eb31983604b70067
luis acerv <l.arr.cerv@gmail.com>
2019-08-06 22:10:45 -0500

    add new post

---
+++ src/routes/posts/2019-08-06-how-to-create-a-bitcoin-api-gql/post.js
@@ -0,0 +1,8 @@
+export default {
+    title: `How to create a simple Bitcoin API with Node.js & GraphQL`,
+    tags: ["bitcoin", "graphql", "tutorial","javascript", "crypto"],
+    spoiler:
+      "Learn How to create a bitcoin API using GraphqQL",
+    getContent: () => import("./document.mdx")
+  };
+  
